e7673b18e9ad63b86fe95b6fdbc0591f3b183687
Daniel Romero <52681884+dafero753@users.noreply.github.com>
2020-04-12 12:39:52 -0500

    Updates

---
+++ precache-manifest.5703faa17c2eb8e531e4510a6e4922ce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ea149bd5b51e80b5af43e2ed903530",
+    "url": "/.github.io/yoga/index.html"
+  },
+  {
+    "revision": "fc7d45f1926706b199c3",
+    "url": "/.github.io/yoga/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "38127036477426b33b89",
+    "url": "/.github.io/yoga/static/css/main.f32c61e4.chunk.css"
+  },
+  {
+    "revision": "fc7d45f1926706b199c3",
+    "url": "/.github.io/yoga/static/js/2.f8832d1e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/.github.io/yoga/static/js/2.f8832d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38127036477426b33b89",
+    "url": "/.github.io/yoga/static/js/main.264f9b20.chunk.js"
+  },
+  {
+    "revision": "a5d92df0c0e9e28f195a",
+    "url": "/.github.io/yoga/static/js/runtime-main.dee7d23d.js"
+  },
+  {
+    "revision": "fbf3d6b2444c953b2889fd63722f5447",
+    "url": "/.github.io/yoga/static/media/1.fbf3d6b2.png"
+  },
+  {
+    "revision": "fd20b728917b64e14cbb093aaffa1293",
+    "url": "/.github.io/yoga/static/media/2.fd20b728.png"
+  },
+  {
+    "revision": "961081a93c714fbbb3e615d2048dbe56",
+    "url": "/.github.io/yoga/static/media/Captura.961081a9.PNG"
+  },
+  {
+    "revision": "ae818f685b908d788f3715ddc3fc8534",
+    "url": "/.github.io/yoga/static/media/Captura2.ae818f68.PNG"
+  },
+  {
+    "revision": "a700a3cd68d9df461df49de155fa19f3",
+    "url": "/.github.io/yoga/static/media/Captura3.a700a3cd.PNG"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/.github.io/yoga/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "8f35a40403a84631c4125c4f1859c7a6",
+    "url": "/.github.io/yoga/static/media/twitter.8f35a404.png"
+  }
+]);
